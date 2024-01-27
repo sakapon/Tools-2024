b4ad329634ac27fb941ebed46750b7bb15637048
Keiho Sakapon <git@saka-pon.net>
2024-01-27 15:06:05 +0900

    Add StackDFS class

--- BfsDfs/BfsDfsViewer/BfsDfs.cs
+++ BfsDfs/BfsDfsViewer/BfsDfs.cs
@@ -8,14 +8,14 @@ namespace BfsDfsViewer
 		public ReactiveProperty<string> Color { get; } = new ReactiveProperty<string>();
 	}
 
-	public class BFS
+	public class QueueBFS
 	{
 		readonly int h, w;
 		public int Height => h;
 		public int Width => w;
 		public Cell[] Cells { get; }
 
-		public BFS(int h, int w)
+		public QueueBFS(int h, int w)
 		{
 			this.h = h;
 			this.w = w;
@@ -46,6 +46,64 @@ namespace BfsDfsViewer
 					if (Cells[nv].Cost.Value.HasValue) continue;
 					Cells[nv].Cost.Value = nc;
 					q.Enqueue(nv);
+
+					Cells[nv].Color.Value = MainViewModel.Color_Queued;
+					Thread.Sleep(MainViewModel.TimeInterval);
+				}
+
+				Cells[v].Color.Value = MainViewModel.Color_End;
+				Thread.Sleep(MainViewModel.TimeInterval);
+			}
+		}
+
+		IEnumerable<int> GetNexts(int v)
+		{
+			var (i, j) = (v / w, v % w);
+			if (j > 0) yield return v - 1;
+			if (j + 1 < w) yield return v + 1;
+			if (i > 0) yield return v - w;
+			if (i + 1 < h) yield return v + w;
+		}
+	}
+
+	public class StackDFS
+	{
+		readonly int h, w;
+		public int Height => h;
+		public int Width => w;
+		public Cell[] Cells { get; }
+
+		public StackDFS(int h, int w)
+		{
+			this.h = h;
+			this.w = w;
+			Cells = Array.ConvertAll(new bool[h * w], _ => new Cell());
+		}
+
+		public void Execute(int sv)
+		{
+			Thread.Sleep(MainViewModel.TimeInterval);
+
+			var q = new Stack<int>();
+			Cells[sv].Cost.Value = 0;
+			q.Push(sv);
+
+			Cells[sv].Color.Value = MainViewModel.Color_Queued;
+			Thread.Sleep(MainViewModel.TimeInterval);
+
+			while (q.Count > 0)
+			{
+				var v = q.Pop();
+				var nc = Cells[v].Cost.Value + 1;
+
+				Cells[v].Color.Value = MainViewModel.Color_Current;
+				//Thread.Sleep(MainViewModel.TimeInterval);
+
+				foreach (var nv in GetNexts(v))
+				{
+					if (Cells[nv].Cost.Value.HasValue) continue;
+					Cells[nv].Cost.Value = nc;
+					q.Push(nv);
 
 					Cells[nv].Color.Value = MainViewModel.Color_Queued;
 					Thread.Sleep(MainViewModel.TimeInterval);
--- BfsDfs/BfsDfsViewer/MainViewModel.cs
+++ BfsDfs/BfsDfsViewer/MainViewModel.cs
@@ -4,14 +4,15 @@
 	{
 		public const int Height = 11;
 		public const int Width = 15;
-		public const int Start = Height * Width / 2;
+		public const int StartId = Height * Width / 2;
 		public const int TimeInterval = 100;
 
 		public const string Color_Current = "#FF3333";
 		public const string Color_Queued = "#FF9933";
 		public const string Color_End = "#33AA33";
 
-		public BFS BFS { get; } = new BFS(Height, Width);
+		public QueueBFS QueueBFS { get; } = new QueueBFS(Height, Width);
+		public StackDFS StackDFS { get; } = new StackDFS(Height, Width);
 
 		public MainViewModel()
 		{
@@ -20,7 +21,7 @@
 
 		public void Execute()
 		{
-			BFS.Execute(Start);
+			QueueBFS.Execute(StartId);
 		}
 	}
 }
