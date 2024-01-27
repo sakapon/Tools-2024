7dc0a08647d17ed0fed0e583ee719368e0581ffa
Keiho Sakapon <git@saka-pon.net>
2024-01-27 12:09:10 +0900

    Update code

--- BfsDfs/BfsDfsViewer/MainViewModel.cs
+++ BfsDfs/BfsDfsViewer/MainViewModel.cs
@@ -9,10 +9,16 @@ namespace BfsDfsViewer
 		public const int Start = Height * Width / 2;
 		public const int TimeInterval = 100;
 
-		public const string Color_Queued = "#3399FF";
-		public const string Color_End = "#FF9933";
+		public const string Color_Current = "#FF3333";
+		public const string Color_Queued = "#FF9933";
+		public const string Color_End = "#33AA33";
 
 		public BFS BFS { get; } = new BFS(Height, Width);
+
+		public MainViewModel()
+		{
+			Task.Run(() => Execute());
+		}
 
 		public void Execute()
 		{
@@ -31,7 +37,7 @@ namespace BfsDfsViewer
 		{
 			this.h = h;
 			this.w = w;
-			Cells = Enumerable.Range(0, h * w).Select(v => new Cell()).ToArray();
+			Cells = Array.ConvertAll(new bool[h * w], _ => new Cell());
 		}
 
 		public void Execute(int sv)
@@ -40,25 +46,31 @@ namespace BfsDfsViewer
 
 			var q = new Queue<int>();
 			Cells[sv].Cost.Value = 0;
-			Cells[sv].Color.Value = MainViewModel.Color_Queued;
 			q.Enqueue(sv);
+
+			Cells[sv].Color.Value = MainViewModel.Color_Queued;
 			Thread.Sleep(MainViewModel.TimeInterval);
 
 			while (q.Count > 0)
 			{
 				var v = q.Dequeue();
-				Cells[v].Color.Value = MainViewModel.Color_End;
-				Thread.Sleep(MainViewModel.TimeInterval);
-
 				var nc = Cells[v].Cost.Value + 1;
+
+				Cells[v].Color.Value = MainViewModel.Color_Current;
+				//Thread.Sleep(MainViewModel.TimeInterval);
+
 				foreach (var nv in GetNexts(v))
 				{
 					if (Cells[nv].Cost.Value.HasValue) continue;
 					Cells[nv].Cost.Value = nc;
-					Cells[nv].Color.Value = MainViewModel.Color_Queued;
 					q.Enqueue(nv);
+
+					Cells[nv].Color.Value = MainViewModel.Color_Queued;
 					Thread.Sleep(MainViewModel.TimeInterval);
 				}
+
+				Cells[v].Color.Value = MainViewModel.Color_End;
+				Thread.Sleep(MainViewModel.TimeInterval);
 			}
 		}
 
