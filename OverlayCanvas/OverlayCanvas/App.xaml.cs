c5be6c5c64904fd0ac313b3e56574c72f4974ca8
Keiho Sakapon <git@saka-pon.net>
2024-09-24 20:13:06 +0900

    Create project

---
+++ OverlayCanvas/OverlayCanvas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OverlayCanvas
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
