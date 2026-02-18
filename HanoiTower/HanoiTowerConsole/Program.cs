1cd281bce5aa6595f53158296b3bfdb2839f3019
Keiho Sakapon <git@saka-pon.net>
2026-02-18 23:01:14 +0900

    Create project

---
+++ HanoiTower/HanoiTowerConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HanoiTowerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
