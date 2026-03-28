d28e65b1fd374126cbe9ba511b64882b67e20d50
Keiho Sakapon <git@saka-pon.net>
2026-03-28 23:12:02 +0900

    Add Disk class

---
+++ HanoiTower/HanoiTowerWpf101/Disk.cs
@@ -0,0 +1,25 @@
+﻿using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace HanoiTowerWpf101
+{
+	public class Disk
+	{
+		public int Id { get; }
+		public ReactiveProperty<int> TowerId { get; }
+		public ReactiveProperty<int> Index { get; }
+
+		public ReadOnlyReactiveProperty<int> Left { get; }
+		public ReadOnlyReactiveProperty<int> Top { get; }
+
+		public Disk(int id, int towerId, int index)
+		{
+			Id = id;
+			TowerId = new ReactiveProperty<int>(towerId);
+			Index = new ReactiveProperty<int>(index);
+
+			Left = TowerId.Select(x => x * 100).ToReadOnlyReactiveProperty();
+			Top = Index.Select(x => x * 20).ToReadOnlyReactiveProperty();
+		}
+	}
+}
