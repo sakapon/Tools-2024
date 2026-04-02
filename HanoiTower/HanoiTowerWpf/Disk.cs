0a26ea839765e6cec22fe153430d81754f062553
Keiho Sakapon <git@saka-pon.net>
2026-04-02 10:12:02 +0900

    Copy project

---
+++ HanoiTower/HanoiTowerWpf/Disk.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace HanoiTowerWpf101
+{
+	public class Disk
+	{
+		public int Id { get; }
+		public ReactiveProperty<int> TowerId { get; }
+		public ReactiveProperty<int> Index { get; }
+
+		public int Width => Id * 20;
+		public ReadOnlyReactiveProperty<int> Left { get; }
+		public ReadOnlyReactiveProperty<int> Top { get; }
+
+		public Disk(int id, int towerId, int index)
+		{
+			Id = id;
+			TowerId = new ReactiveProperty<int>(towerId);
+			Index = new ReactiveProperty<int>(index);
+
+			Left = TowerId.Select(x => x * 200).ToReadOnlyReactiveProperty();
+			Top = Index.Select(x => x * 25).ToReadOnlyReactiveProperty();
+		}
+	}
+}
