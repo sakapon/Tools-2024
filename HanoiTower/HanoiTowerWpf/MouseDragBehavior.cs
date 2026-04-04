4a31fe542362dd4a2af07797f0f15df00ef5e5ee
Keiho Sakapon <git@saka-pon.net>
2026-04-04 14:30:47 +0900

    Use Vector

--- HanoiTower/HanoiTowerWpf/MouseDragBehavior.cs
+++ HanoiTower/HanoiTowerWpf/MouseDragBehavior.cs
@@ -1,17 +1,17 @@
 ﻿using System.Windows;
-using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.Xaml.Behaviors;
 
 namespace HanoiTowerWpf
 {
-	public class MouseDragBehavior : Behavior<UIElement>
+	public class MouseDragBehavior : Behavior<FrameworkElement>
 	{
 		public static readonly DependencyProperty DeltaProperty =
-			DependencyProperty.Register("Delta", typeof(Point), typeof(MouseDragBehavior), new FrameworkPropertyMetadata(default(Point), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnDeltaChanged));
+			DependencyProperty.Register("Delta", typeof(Vector), typeof(MouseDragBehavior), new FrameworkPropertyMetadata(default(Vector), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnDeltaChanged));
 
-		public Point Delta
+		public Vector Delta
 		{
-			get { return (Point)GetValue(DeltaProperty); }
+			get { return (Vector)GetValue(DeltaProperty); }
 			set { SetValue(DeltaProperty, value); }
 		}
 
@@ -19,17 +19,34 @@ namespace HanoiTowerWpf
 		{
 			var behavior = (MouseDragBehavior)d;
 			var ue = behavior.AssociatedObject;
-			var delta = (Point)e.NewValue;
+			var delta = (Vector)e.NewValue;
 		}
 
 		protected override void OnAttached()
 		{
 			base.OnAttached();
-		}
 
-		protected override void OnDetaching()
-		{
-			base.OnDetaching();
+			// アタッチされたタイミングでは構造が確定していないため、Loaded イベントで登録します。
+			var fe = AssociatedObject;
+			fe.Loaded += (_, _) =>
+			{
+				var tt = (TranslateTransform)fe.FindName("DragTransform");
+
+				fe.MouseLeftButtonDown += (_, e) =>
+				{
+					var p = e.GetPosition(null);
+				};
+				fe.MouseMove += (_, e) =>
+				{
+					var p = e.GetPosition(null);
+				};
+				fe.MouseLeftButtonUp += (_, e) =>
+				{
+				};
+				fe.MouseLeave += (_, e) =>
+				{
+				};
+			};
 		}
 	}
 }
