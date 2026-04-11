1c3a6e50997d535ee37223192bb429feefda46de
Keiho Sakapon <git@saka-pon.net>
2026-04-11 23:35:47 +0900

    Copy project

---
+++ HanoiTower/HanoiTowerWpf201/AppModel.cs
@@ -0,0 +1,47 @@
+﻿using Reactive.Bindings;
+
+namespace HanoiTowerWpf101
+{
+	public class AppModel
+	{
+		const int NumberOfDisks = 6;
+
+		readonly Stack<Disk>[] Towers =
+		[
+			new Stack<Disk>(Enumerable.Range(1, NumberOfDisks).Reverse().Select((id, index) => new Disk(id, 0, index))),
+			new Stack<Disk>(),
+			new Stack<Disk>(),
+		];
+
+		public Disk[] Disks { get; }
+		public ReactiveProperty<int> Count { get; } = new ReactiveProperty<int>(0);
+
+		public AppModel()
+		{
+			Disks = [.. Towers[0]];
+			Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+				MoveTower(NumberOfDisks, 0, 2, 1);
+			});
+		}
+
+		void MoveTower(int n, int from, int to, int via)
+		{
+			--n;
+			if (n > 0) MoveTower(n, from, via, to);
+			MoveDisk(from, to);
+			if (n > 0) MoveTower(n, via, to, from);
+		}
+
+		void MoveDisk(int from, int to)
+		{
+			Thread.Sleep(400);
+			var disk = Towers[from].Pop();
+			disk.TowerId.Value = to;
+			disk.IndexInTower.Value = Towers[to].Count;
+			Towers[to].Push(disk);
+			Count.Value++;
+		}
+	}
+}
---
+++ HanoiTower/HanoiTowerWpf201/Disk.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace HanoiTowerWpf101
+{
+	public class Disk
+	{
+		public int Id { get; }
+		public ReactiveProperty<int> TowerId { get; }
+		public ReactiveProperty<int> IndexInTower { get; }
+
+		public int Width => Id * 30 + 10;
+		public ReadOnlyReactiveProperty<int> Left { get; }
+		public ReadOnlyReactiveProperty<int> Top { get; }
+
+		public Disk(int id, int towerId, int index)
+		{
+			Id = id;
+			TowerId = new ReactiveProperty<int>(towerId);
+			IndexInTower = new ReactiveProperty<int>(index);
+
+			Left = TowerId.Select(x => x * 290).ToReadOnlyReactiveProperty();
+			Top = IndexInTower.Select(x => (x + 1) * -32).ToReadOnlyReactiveProperty();
+		}
+	}
+}
