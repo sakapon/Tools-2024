cdd37a1537e79a4a0dd1190ac28e9d8dc0ab4db9
Keiho Sakapon <git@saka-pon.net>
2026-04-11 21:14:46 +0900

    Update code by HanoiTowerWpf

--- HanoiTower/HanoiTowerWpf101/Disk.cs
+++ HanoiTower/HanoiTowerWpf101/Disk.cs
@@ -7,9 +7,9 @@ namespace HanoiTowerWpf101
 	{
 		public int Id { get; }
 		public ReactiveProperty<int> TowerId { get; }
-		public ReactiveProperty<int> Index { get; }
+		public ReactiveProperty<int> IndexInTower { get; }
 
-		public int Width => Id * 20;
+		public int Width => Id * 30 + 10;
 		public ReadOnlyReactiveProperty<int> Left { get; }
 		public ReadOnlyReactiveProperty<int> Top { get; }
 
@@ -17,10 +17,10 @@ namespace HanoiTowerWpf101
 		{
 			Id = id;
 			TowerId = new ReactiveProperty<int>(towerId);
-			Index = new ReactiveProperty<int>(index);
+			IndexInTower = new ReactiveProperty<int>(index);
 
-			Left = TowerId.Select(x => x * 200).ToReadOnlyReactiveProperty();
-			Top = Index.Select(x => x * 25).ToReadOnlyReactiveProperty();
+			Left = TowerId.Select(x => x * 290).ToReadOnlyReactiveProperty();
+			Top = IndexInTower.Select(x => (x + 1) * -32).ToReadOnlyReactiveProperty();
 		}
 	}
 }
