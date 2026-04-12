72242587751f361aa9d46e84a3dd3c0d06d7ffc3
Keiho Sakapon <git@saka-pon.net>
2026-04-12 09:51:15 +0900

    Move class

--- HanoiTower/HanoiTowerWpf201/AppModel.cs
+++ HanoiTower/HanoiTowerWpf201/AppModel.cs
@@ -44,4 +44,13 @@ namespace HanoiTowerWpf201
 			Count.Value++;
 		}
 	}
+
+	public class Disk(int id, int towerId, int index)
+	{
+		public int Id { get; } = id;
+		public ReactiveProperty<int> TowerId { get; } = new ReactiveProperty<int>(towerId);
+		public ReactiveProperty<int> IndexInTower { get; } = new ReactiveProperty<int>(index);
+
+		public int Width => Id * 30 + 10;
+	}
 }
