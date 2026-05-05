749be9fa7047685941a401c2431ffcce836dd491
Keiho Sakapon <git@saka-pon.net>
2026-05-05 19:26:36 +0900

    Create OpenSilver project

---
+++ HanoiTower-OS/HanoiTowerOS102/HanoiTowerOS102/HanoiTowerOS102/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HanoiTowerOS102
+{
+	public sealed partial class App : Application
+	{
+		public App()
+		{
+			this.InitializeComponent();
+
+			// Enter construction logic here...
+
+			var mainPage = new MainPage();
+			this.RootVisual = mainPage;
+		}
+	}
+}
