1163e683e4177433369b7c0aafb50b021e85ae81
Keiho Sakapon <git@saka-pon.net>
2026-05-09 23:03:26 +0900

    Update layout

--- HanoiTower-OS/HanoiTowerOS201/HanoiTowerOS201/HanoiTowerOS201/MainPage.xaml.cs
+++ HanoiTower-OS/HanoiTowerOS201/HanoiTowerOS201/HanoiTowerOS201/MainPage.xaml.cs
@@ -16,4 +16,11 @@ namespace HanoiTowerOS201
 			// Enter construction logic here...
 		}
 	}
+
+	public static class ViewFuncs
+	{
+		public static readonly Func<int, int> Disk_Id_Width = id => id * 30 + 10;
+		public static readonly Func<int, int> Disk_TowerId_X = id => id * 290;
+		public static readonly Func<int, int> Disk_Index_Y = i => (i + 1) * -32;
+	}
 }
